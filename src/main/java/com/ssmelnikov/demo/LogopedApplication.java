a685a16080cc637b23cb8034aee0671bc507026f
s.melnikov <s.melnikov@bpcbt.com>
2018-10-08 19:15:48 +0300

    First template.

--- src/main/java/com/ssmelnikov/demo/LogopedApplication.java
+++ src/main/java/com/ssmelnikov/demo/LogopedApplication.java
@@ -1,5 +1,7 @@
 package com.ssmelnikov.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -7,13 +9,23 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class LogopedApplication extends SpringBootServletInitializer {
+	private static final Logger logger = LoggerFactory.getLogger(LogopedApplication.class);
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(LogopedApplication.class);
-	}
+        try {
+            return application.sources(LogopedApplication.class);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
+        return null;
+    }
 
 	public static void main(String[] args) {
-		SpringApplication.run(LogopedApplication.class, args);
-	}
+        try {
+            SpringApplication.run(LogopedApplication.class, args);
+        } catch (Exception e) {
+            logger.error("", e);
+        }
+    }
 }
